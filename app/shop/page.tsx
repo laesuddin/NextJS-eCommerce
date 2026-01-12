a539707020c40ae356eb017d224fc6951c152571
Laes Uddin <laesuddin@gmail.com>
2026-01-13 03:13:09 +0600

    HeaderMenu design done

---
+++ app/shop/page.tsx
@@ -0,0 +1,10 @@
+import Container from '@/components/Container'
+import React from 'react'
+
+const ShopPage = () => {
+  return (
+    <Container>ShopPage</Container>
+  )
+}
+
+export default ShopPage
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,17 @@
-import Container from "./Container"
-import Logo from "./Logo"
+import Container from "./Container";
+import HeaderMenu from "./HeaderMenu";
+import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header className="bg-white py-5">
-        <Container>
-            {/* logo */}
-            <Logo />
-            {/* NavButton */}
-            {/* NavAdmin */}
-        </Container>
+    <header className="bg-white py-5 border-b border-b-black/20">
+      <Container className="flex items-center justify-between">
+        <Logo />
+        <HeaderMenu />
+        <div>others</div>
+      </Container>
     </header>
-  ) 
-}
- 
-export default Header
+  );
+};
+
+export default Header;
